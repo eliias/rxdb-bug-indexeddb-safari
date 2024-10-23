baf4c1fccda67d97cd1c1afaff304f92542e62d1
Hannes Moser <box@hannesmoser.at>
2024-10-23 14:46:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# React + Vite
+
+## Getting started
+
+```bash
+echo "RXDB_PREMIUM=${RXDB_PREMIUM}" >> .env
+```
+
+```bash
+pnpm install
+pnpm dev
+```
---
+++ src/App.jsx
@@ -0,0 +1,96 @@
+import {useEffect, useState} from 'react'
+import {createRxDatabase} from 'rxdb';
+import {getRxStorageSharedWorker} from "rxdb-premium/plugins/storage-worker";
+import {faker} from '@faker-js/faker';
+
+import './App.css'
+
+const storage = getRxStorageSharedWorker({
+    workerInput: "/indexeddb.worker.js",
+    workerOptions: {
+        name: "heroes",
+        type: "module",
+        credentials: "omit",
+    },
+});
+
+const database = (async () => {
+    const schema = {
+        "title": "hero schema",
+        "version": 0,
+        "description": "describes a simple hero",
+        "primaryKey": "name",
+        "type": "object",
+        "properties": {
+            "name": {
+                "type": "string",
+                "maxLength": 100
+            }
+        },
+        "required": [
+            "name",
+        ]
+    };
+    const db = await createRxDatabase({
+        name: "heroesdb",
+        storage: storage,
+        multiInstance: true,
+        eventReduce: true,
+        cleanupPolicy: {}
+    });
+    await db.addCollections({
+        documents: {
+            schema: schema
+        }
+    });
+    const data = await db.collections.documents.find().exec();
+    if (data.length === 0) {
+        const documents = [];
+        const collection = db.documents;
+        for (let i = 1; i < 1_000; i += 1) {
+            documents.push({
+                name: faker.person.fullName()
+            });
+        }
+        await collection.bulkInsert(documents);
+    }
+
+    return db;
+})();
+
+function App() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            const db = await database;
+            const data = await db.collections.documents.find().exec();
+            setData(data);
+        })();
+    }, []);
+
+    function renderRecord(record) {
+        return <tr key={record.name}>
+            <td>{record.name}</td>
+        </tr>
+    }
+
+    return (
+        <>
+            <h1>RxDB + IndexedDB ❤️‍🩹Safari</h1>
+            <table>
+                <thead>
+                <tr>
+                    <td>Name</td>
+                </tr>
+                </thead>
+                <tbody>
+                {data && data.map(renderRecord)}
+                </tbody>
+            </table>
+
+        </>
+    )
+}
+
+export default App
